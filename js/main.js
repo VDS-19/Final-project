d4898776fdcb660ad0a5555d74abc1b9cef68680
Peihong Man <30327122+manpeihong@users.noreply.github.com>
2019-11-14 11:29:45 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 var App = App || {};
 
+var colorSequence = d3.scaleSequential(d3.interpolateBlues).domain([0, 100]);
+var orangeSequence = d3.scaleSequential(d3.interpolateOranges).domain([0, 100]);
+
+var points, plane, starsGeometry;
+
 (function() {
 
     // setup the pointer to the scope 'this' variable
@@ -20,7 +25,6 @@ var App = App || {};
 
         // render the scene
         App.scene.render();
-		console.log("hello");
 
     };
 
@@ -74,6 +78,167 @@ const Scene = function(options) {
 	return self.public;
 };
 const ParticleSystem = function() {
+	// setup the pointer to the scope 'this' variable
+    const self = this;
+
+    // data container
+    const data = [];
+
+    // scene graph group for the particle system
+    const sceneObject = new THREE.Group();
+
+    // bounds of the data
+	const bounds = {};
+	// var bounds = {};
+
+    var planexScale = d3.scaleLinear()
+        .range([0, 500]);
+
+    var planeyScale = d3.scaleLinear()
+		.range([500, 0]);
+
+	self.loadData = function(file){
+			
+		// read the csv file
+		d3.csv(file)
+		// iterate over the rows of the csv file
+		.row(function(d) {
+
+			// get the min bounds
+			bounds.minX = Math.min(bounds.minX || Infinity, d.X);
+			bounds.minY = Math.min(bounds.minY || Infinity, d.Y);
+			bounds.minZ = Math.min(bounds.minZ || Infinity, d.Z);
+		
+			// get the max bounds
+			bounds.maxX = Math.max(bounds.maxX || -Infinity, d.X);
+			bounds.maxY = Math.max(bounds.maxY || -Infinity, d.Y);
+			bounds.maxZ = Math.max(bounds.maxY || -Infinity, d.Z);
+
+			// add the element to the data collection
+			data.push({
+				// Position
+				
+				X: Number(d.X),
+				Y: Number(d.Y),
+				Z: Number(d.Z),
+				// Velocity
+				U: Number(d.Vx),
+				V: Number(d.Vy),
+				W: Number(d.Vz),
+				
+				type: d.Type,
+				system: d.System
+			});
+		})
+		// when done loading
+		.get(function() {
+			// draw the containment cylinder
+			// TODO: Remove after the data has been rendered
+			// self.drawContainment();
+			
+			// create legend
+			// self.createlegend();
+			// create the particle system
+			self.createParticleSystem();
+			// create the 2D plane
+			// self.createplane(0);
+		});
+	};
+	self.createParticleSystem = function() {
+		starsGeometry = new THREE.BufferGeometry()
+        var positions = [];
+		var colors = [];
+		
+        for(var i = 0; i < data.length;i ++ )
+        {
+			console.log("hellow!");
+			positions.push(data[i]['X'],data[i]['Y'],data[i]['Z']);
+			console.log(data[i]['X'])
+            var color = new THREE.Color();
+			// color.set(colorSequence(data[i]['X']));
+			
+			color.set(colorSequence(10));	// Some random number for now
+			console.log(color);
+			colors.push(color.r, color.g, color.b);
+			// colors.push(100, 200, 10)
+		} 
+        starsGeometry.addAttribute( 'position', new THREE.BufferAttribute( new Float32Array(positions), 3 ) );
+        starsGeometry.addAttribute( 'color', new THREE.BufferAttribute( new Float32Array(colors), 3 ) );
+        starsGeometry.computeBoundingSphere();
+		
+
+        var starsMaterial = new THREE.PointsMaterial( { size: 0.1, vertexColors: THREE.VertexColors, opacity:0.7, transparent:true } );
+        points = new THREE.Points( starsGeometry, starsMaterial );
+        points.position.set(0, -7, 0)
+		sceneObject.add(points);
+		// var starsMaterial = new THREE.PointsMaterial( {size:0.1,vertexColors: true, side:THREE.DoubleSide, sizeAttenuation: true} );
+		// var starField = new THREE.Points( starsGeometry, starsMaterial );
+		// sceneObject.add( starField );
+
+
+        var plane_geometry = new THREE.PlaneGeometry(15,15);
+        var plane_material = new THREE.MeshBasicMaterial( {color: 0x1C7AF2, side: THREE.DoubleSide, transparent:true, opacity:0.9 } );
+        plane = new THREE.Mesh( plane_geometry, plane_material );
+        plane.position.set(0, -2, 0)
+		sceneObject.add( plane );
+			
+			
+	};
+
+	// creates the plane
+    self.createplane = function(z) {
+        
+        $(".planeDiv").html('');
+        var points_arr = [];
+        for(var i = 0; i<data.length;i++)
+        {
+            if((data[i]['Z'] >= z-0.1) && (data[i]['Z'] < z+0.1))
+            {
+                points_arr.push({"X": data[i]['X'], "Y": data[i]['Y'],"concentration":data[i]['concentration']});
+            }    
+        }
+
+        //Create an SVG
+        var plane_svg = d3.select(".planeDiv").append("svg")
+            .attr("width", 800)
+            .attr("height", 550)
+            .append("g")
+            .attr("transform", "translate(5,5)");   
+
+        planexScale.domain([bounds.minX, bounds.maxX]);
+        planeyScale.domain([bounds.minY, bounds.maxY]);
+
+        plane_svg.selectAll('circle')
+            .data(points_arr)
+            .enter()
+            .append('circle')
+            .attr('class','point_value')
+            .attr("r", 2.5)
+            .attr('cx', function(d) { return planexScale(d.X); })
+            .attr('cy', function(d) { return planeyScale(d.Y); })
+            .style('fill', function(d) { return colorSequence(d.concentration); });   
+
+	};
+
+	self.public = {
+	
+	    // load the data and setup the system
+	    initialize: function(file){
+	        self.loadData(file);
+			
+	    },
+	
+	    // accessor for the particle system
+	    getParticleSystems : function() {
+	        return sceneObject;
+	    }
+	};
+	
+	return self.public;
+	
+};
+
+const ParticleSystem2 = function() {
 	const sceneObject = new THREE.Group();
 	const data = [];
 	// setup the pointer to the scope 'this' variable
